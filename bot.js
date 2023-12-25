cdf0fbc34b2a846e46a00bd2ebd2b04556a79286
Lucas Nielsen <97773113+lucasnielsen@users.noreply.github.com>
2023-12-25 10:35:46 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const { Client, GatewayIntentBits, PermissionFlagsBits, ApplicationCommandType, ApplicationCommandOptionType } = require('discord.js');
 const fs = require('fs');
 
-// Bot setup
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -11,22 +10,25 @@ const client = new Client({
     ]
 });
 
-// Store the message file
 const responseMessageFile = 'responseMessage.txt';
 
-// Save the message
+// authorized user IDs
+const authorizedIDs = ['601274881954414612', '351535390618026004'];
+
+// cooldown tracking
+const cooldowns = new Map();
+const cooldownSeconds = 15; // cooldown
+
 function saveResponseMessage(message) {
     fs.writeFileSync(responseMessageFile, message);
 }
 
-// Load the message
 function loadResponseMessage() {
     if (fs.existsSync(responseMessageFile)) {
         return fs.readFileSync(responseMessageFile, 'utf8');
     }
-    return "{mention} DON'T SELF STAR";
+    return "Self-starring is not allowed here!";
 }
-
 
 let responseMessage = loadResponseMessage();
 
@@ -34,6 +36,20 @@ client.once('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.guilds.cache.forEach(async guild => {
         try {
+            // register the /status command
+            await guild.commands.create({
+                name: 'status',
+                description: 'Change the bot\'s status message',
+                type: ApplicationCommandType.ChatInput,
+                options: [{
+                    name: 'message',
+                    type: ApplicationCommandOptionType.String,
+                    description: 'The new status message',
+                    required: true,
+                }]
+            });
+
+            // register the /changemessage command
             await guild.commands.create({
                 name: 'changemessage',
                 description: 'Change the message sent when someone self-stars.',
@@ -53,24 +69,43 @@ client.once('ready', async () => {
 });
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand() || interaction.commandName !== 'changemessage') return;
+    if (!interaction.isCommand()) return;
 
-    if (interaction.member.permissions.has(PermissionFlagsBits.ManageGuild)) {
-        responseMessage = interaction.options.getString('message');
-        saveResponseMessage(responseMessage);
-        await interaction.reply(`Response message changed to: ${responseMessage}`);
-    } else {
-        await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+    if (interaction.commandName === 'changemessage') {
+        if (interaction.member.permissions.has(PermissionFlagsBits.ManageGuild)) {
+            responseMessage = interaction.options.getString('message');
+            saveResponseMessage(responseMessage);
+            await interaction.reply(`Response message changed to: ${responseMessage}`);
+        } else {
+            await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+        }
+    }
+
+    if (interaction.commandName === 'status') {
+        if (authorizedIDs.includes(interaction.user.id)) {
+            const newStatus = interaction.options.getString('message');
+            client.user.setActivity(newStatus);
+            await interaction.reply(`Bot status updated to: ${newStatus}`);
+        } else {
+            await interaction.reply({ content: 'You are not authorized to use this command.', ephemeral: true });
+        }
     }
 });
 
 client.on('messageReactionAdd', async (reaction, user) => {
     if (reaction.emoji.name === '⭐' && reaction.message.author.id === user.id) {
-        const personalizedMessage = responseMessage.replace('{mention}', `<@${user.id}>`);
-        reaction.message.channel.send(personalizedMessage);
         reaction.users.remove(user);
+
+        const currentTime = Date.now();
+        const lastUsed = cooldowns.get(user.id);
+
+        // send the message only if the user is not in the cooldown period
+        if (!lastUsed || currentTime - lastUsed > cooldownSeconds * 1000) {
+            cooldowns.set(user.id, currentTime);
+            const personalizedMessage = responseMessage.replace('{mention}', `<@${user.id}>`);
+            reaction.message.channel.send(personalizedMessage);
+        }
     }
 });
 
-// Replace with bot token
-client.login('token_here');
+client.login('bot_token_here');
